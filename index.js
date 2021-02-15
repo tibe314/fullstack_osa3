b0c932e439b12a160c0869b1318059d9e2f96552
tibe314 <tino.berg@helsinki.fi>
2021-02-15 18:52:38 +0200

    osa3 valmis

--- index.js
+++ index.js
@@ -12,135 +12,122 @@ var morgan = require('morgan')
 app.use(morgan('tiny'))
 
 let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    },
-    {
-        id: 5,
-        name: "Erkki Esimerkki",
-        number: "3-14159265"
-    },
-    {
-        id : 6,
-        name: "Maija Meikäläinen",
-        number: "987654321"
-    }
+  {
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendick',
+    number: '39-23-6423122'
+  },
+  {
+    id: 5,
+    name: 'Erkki Esimerkki',
+    number: '3-14159265'
+  },
+  {
+    id : 6,
+    name: 'Maija Meikäläinen',
+    number: '987654321'
+  }
 ]
-
-//päivämäärä infosivua varten
-const date = Date().toLocaleString()
 
 //kaikki henkilöt
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 //yksittäisen henkilön lisäys
 //ei kelpaa jos: nimi/numero puuttuu, nimi ei ole uniikki
 app.post('/api/persons', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    //tsekkaus
-    if(!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'content is missing'
-        })
-    }
-    var names = persons.map(function(person) {
-        return person.name
+  //tsekkaus
+  if(!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content is missing'
     })
-    if (names.includes(body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-    //tsekkaus tehty, tässä ei pitäisi olla ongelmia
+  }
+  var names = persons.map(function(person) {
+    return person.name
+  })
+  if (names.includes(body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+  //tsekkaus tehty, tässä ei pitäisi olla ongelmia
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-        id: generateId(0, 10000)
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+    id: generateId(0, 10000)
+  })
 
-    person.save(function (error) {
-        console.log(error)
-    }).then(savedPerson => {
-        response.json(savedPerson)
-    })
+  person.save(function (error) {
+    console.log(error)
+  }).then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 //yksittäinen henkilö
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then(person => {
-        if (person) {
-            response.json(note)
-        } else {
-            response.status(404).end()
-        }
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
-  })
+})
 
 // yksittäisen henkilön poisto
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      console.log(result)
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
 //random int id:tä varten
 function generateId(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max-min) + min)
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max-min) + min)
 }
 
 //error handler
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id'})
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
 
-    next(error)
+  next(error)
 }
 app.use(errorHandler)
 
-// /info sivu
-/*
-app.get('/info', (req, res) => {
-    res.send(
-        `<div>
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${date}</p>
-        </div>`
-    )
-})*/
-
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -15,31 +15,31 @@ const url =
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
-  
+
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
     })
+    mongoose.connection.close()
+  })
 } else {
 
-    //console.log("nimi:", process.argv[3])
-    //console.log("numero:", process.argv[4])
+  //console.log("nimi:", process.argv[3])
+  //console.log("numero:", process.argv[4])
 
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
-    })
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
 
-    person.save().then(response => {
-        console.log('person saved!')
-        mongoose.connection.close()
-    })
+  person.save().then(response => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
 }
